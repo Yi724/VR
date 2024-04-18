71c597fb4664f805e5c985fa29dae43cb559e150
Yi724 <160296127+Yi724@users.noreply.github.com>
2024-04-18 11:31:43 +0800

    routine

---
+++ routine/data.js
@@ -0,0 +1,234 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8072810026165644,
+          "pitch": 0.22609562037742315,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6946381828915502,
+          "pitch": -0.14787741521150366,
+          "title": "沿著這條路直走",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "路口轉彎",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.984303326233963,
+          "pitch": 0.05431674942861875,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.708478115083004,
+          "pitch": 0.024093240635833624,
+          "title": "這裡轉彎",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "過馬路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.205798613116613,
+          "pitch": -0.03258001477235872,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1586465496258214,
+          "pitch": -0.031975736278882394,
+          "title": "停車場",
+          "text": ""
+        },
+        {
+          "yaw": -2.371521754319261,
+          "pitch": 0.1300042728432249,
+          "title": "過馬路",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "停車場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.11351106522293,
+          "pitch": -0.05249236620583808,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.26226188220758395,
+          "pitch": -0.09494839177544456,
+          "title": "停車場",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "外面馬路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6492408534773766,
+          "pitch": 0.0941997771715748,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9508601256614124,
+          "pitch": 0.06643261536788359,
+          "title": "圖書館",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
