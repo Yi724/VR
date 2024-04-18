13b9731b8f11bf9efa2525807f21822f8f8edb08
Yi724 <160296127+Yi724@users.noreply.github.com>
2024-04-18 11:22:12 +0800

    library

---
+++ library/data.js
@@ -0,0 +1,252 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "搭電梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1496,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0498999024702016,
+          "pitch": 0.06290442698076504,
+          "rotation": 0,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8527667305775264,
+          "pitch": 0.04326564012510303,
+          "title": "搭電梯",
+          "text": "可到任何樓層"
+        },
+        {
+          "yaw": -0.06293855690454286,
+          "pitch": 0.009301327352641664,
+          "title": "服務台",
+          "text": "辦理借書證"
+        },
+        {
+          "yaw": 2.076049008386094,
+          "pitch": 0.02593448497133899,
+          "title": "露易莎",
+          "text": "肚子餓可以在那邊吃東西"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "5樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1472431821024935,
+          "pitch": 0.06722858016563116,
+          "rotation": 0,
+          "target": "2-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9051964804668398,
+          "pitch": 0.14855496049604966,
+          "title": "影音室",
+          "text": ""
+        },
+        {
+          "yaw": 0.19570229844157083,
+          "pitch": 0.3256303374215008,
+          "title": "閱讀區",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "6樓電梯口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8011246491748576,
+          "pitch": 0.13246536990292057,
+          "rotation": 0,
+          "target": "3-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5332776266476742,
+          "pitch": 0.27551708616868,
+          "title": "閱讀區",
+          "text": "許多學生會在段考前到這裡複習"
+        },
+        {
+          "yaw": -3.0991311206950822,
+          "pitch": 0.28236266826210965,
+          "title": "消毒書櫃",
+          "text": "將借的書放在這裡消毒"
+        }
+      ]
+    },
+    {
+      "id": "3-7",
+      "name": "通往7樓樓梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24995215534610793,
+          "pitch": -0.33022032351114383,
+          "rotation": 0,
+          "target": "4-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.832894046250958,
+          "pitch": 0.09844830417567074,
+          "title": "閱讀區",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-7",
+      "name": "7樓討論室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1217035801549677,
+          "pitch": -0.06881336462715737,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3340575650506956,
+          "pitch": 0.6773768881347415,
+          "title": "討論室",
+          "text": "可於此處討論，但需預約"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
