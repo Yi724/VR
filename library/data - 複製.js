0db849e876a2fcf1a524d6e8fc6da6b88f9dcb0a
Yi724 <160296127+Yi724@users.noreply.github.com>
2024-04-18 11:07:36 +0800

    Create data - 複製.js

---
+++ library/data - 複製.js
@@ -0,0 +1,294 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1496,
+      "initialViewParameters": {
+        "yaw": 1.2275734909621683,
+        "pitch": 0.415288220263605,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1024338180345747,
+          "pitch": 0.11794459350069886,
+          "rotation": 12.566370614359176,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11272061230579666,
+          "pitch": 0.02330072265097627,
+          "title": "服務台",
+          "text": "辦理借書證<div>還書</div><div>拿預約書</div>"
+        },
+        {
+          "yaw": 2.9494856118234196,
+          "pitch": 0.05758247181470644,
+          "title": "電梯",
+          "text": "可到任何樓層"
+        },
+        {
+          "yaw": -2.243383492581458,
+          "pitch": 0.14697590575510056,
+          "title": "閱讀區",
+          "text": "可於此處看書或是休息"
+        },
+        {
+          "yaw": 2.0502882976498285,
+          "pitch": 0.029357181800193644,
+          "title": "路易莎",
+          "text": "可以來此處吃東西"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "5樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0713105242134118,
+          "pitch": 0.16824769590913746,
+          "rotation": 0,
+          "target": "2-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8406246784094815,
+          "pitch": 0.09159626133913434,
+          "title": "影音區",
+          "text": "可以在這邊看影片"
+        },
+        {
+          "yaw": -2.4188792660981626,
+          "pitch": 0.11943913469978007,
+          "title": "休息區",
+          "text": "累了可以在這邊休息"
+        },
+        {
+          "yaw": -1.4245123374885296,
+          "pitch": 0.03384328775294598,
+          "title": "洗手間",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "6樓電梯口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9043026900812485,
+          "pitch": 0.24995774391101477,
+          "rotation": 0,
+          "target": "3-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3895445786157552,
+          "pitch": 0.0935776592701778,
+          "title": "電梯",
+          "text": "可到任何樓層"
+        },
+        {
+          "yaw": 1.7339449025976226,
+          "pitch": 0.1937793329125821,
+          "title": "閱讀區",
+          "text": "看書<div>複習</div>"
+        },
+        {
+          "yaw": -2.6554735552108646,
+          "pitch": 0.22643962369239645,
+          "title": "電腦",
+          "text": "查詢館藏"
+        },
+        {
+          "yaw": -3.036851182711974,
+          "pitch": 0.2808617721876132,
+          "title": "消毒櫃",
+          "text": "可將要借閱的書放在裡面消毒"
+        },
+        {
+          "yaw": 0.8343451319183721,
+          "pitch": 0.17176182398609363,
+          "title": "書櫃",
+          "text": ""
+        },
+        {
+          "yaw": -1.8967368875226054,
+          "pitch": 0.06879293639771333,
+          "title": "樓梯",
+          "text": "通往7樓"
+        }
+      ]
+    },
+    {
+      "id": "3-7",
+      "name": "通往7樓樓梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09160446867189975,
+          "pitch": -0.26648449708616617,
+          "rotation": 0,
+          "target": "4-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.831391655697856,
+          "pitch": 0.08757384090948861,
+          "title": "閱讀區",
+          "text": "段考前，很多學生會來這邊複習"
+        },
+        {
+          "yaw": 0.7787835228314357,
+          "pitch": 0.121301588161149,
+          "title": "樓梯",
+          "text": "通7樓"
+        }
+      ]
+    },
+    {
+      "id": "4-7",
+      "name": "7樓討論室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.195269817979005,
+          "pitch": 0.06807308848198979,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.31204319430007743,
+          "pitch": 0.37687971552053945,
+          "title": "討論室",
+          "text": "咬討論的話可以來這裡，但需要登記"
+        }
+      ]
+    }
+  ],
+  "name": "library",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
